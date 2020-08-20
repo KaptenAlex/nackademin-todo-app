55b40ba86cd415368cf38d7f96f4b48e8fbf7a9a
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-08-20 16:34:16 +0200

    Did some kind of styling

--- views/scripts/read.js
+++ views/scripts/read.js
@@ -2,14 +2,47 @@ fetch('http://localhost:8080/loadAllTodoItems')
 .then(response => response.json())
 .then(data => {
     data.forEach(todoItem => {
+        // Todolist
         let todoListElement = document.getElementById('todoList');
-        let divElement = document.createElement('div');
-        divElement.classList.add('todoItem');
+
+        //Todo item
+        let todoItemElement = document.createElement('div');
+        todoItemElement.classList.add('todoItem');
+
+        //Todo item's title
+        let todoItemTitleElement = document.createElement('p');
+        todoItemTitleElement.innerText = "Title";
+
+        let todoItemTitleInputElement = document.createElement('input');
+        todoItemTitleInputElement.value = todoItem.title;
+
+        //Todo item's content
+        let todoItemContentElement = document.createElement('p');
+        todoItemContentElement.innerText = "Content";
+
+        let todoItemContentInputElement = document.createElement('input');
+        todoItemContentInputElement.value = todoItem.content;
+
+        //Todo item's edit button
         let editButton = document.createElement('button');
+        editButton.classList.add('editTodoItem');
+        editButton.innerText = 'Edit todo item';
+        
+        //Todo item's delete button
         let deleteButton = document.createElement('button');
-        divElement.innerHTML = '<h2>'+ todoItem.title +'</h2><p>'+ todoItem.content +'</p>' + '<br/>' + '<button';
-        todoListElement.appendChild(divElement);
-        //document.body.appendChild(divElement);
+        deleteButton.classList.add('deleteTodoItem');
+        deleteButton.innerText = 'Delete todo item';
+        
+        todoListElement.appendChild(todoItemElement);
+
+        todoItemElement.appendChild(todoItemTitleElement);
+        todoItemElement.appendChild(todoItemTitleInputElement);
+
+        todoItemElement.appendChild(todoItemContentElement);
+        todoItemElement.appendChild(todoItemContentInputElement);
+
+        todoItemElement.appendChild(editButton);
+        todoItemElement.appendChild(deleteButton);
     });
 
 });
