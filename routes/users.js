9a651c3583e62be81c44f7bddc05a2c6e7e70756
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-08-31 10:34:51 +0200

    Removed comment from users route

--- routes/users.js
+++ routes/users.js
@@ -12,11 +12,6 @@ router.delete('/', authorizationController.authorize, usersController.removeUser
 
 // Admin/Users routers
 
-
-// If the user has the role admin, check all todos items the user has
-// If the user has the role user, check only their own todo items
-// router.get('/:userId/todos', authorizationController.authorize, userController.getUserTodoItems);
-
 // Anonymous routers
 router.post("/signin", authorizationController.login);
 router.get("/authorize", authorizationController.authorize, usersController.userIsAuthorized);
