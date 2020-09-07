77b26f3ccfdfce7a2ae20af7ec8c6492052b1102
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-09-07 10:50:04 +0200

    Made tests only test one function, not several

--- test/todoList.test.js
+++ test/todoList.test.js
@@ -2,12 +2,13 @@ const todoListModel = require('../models/todoList.js');
 const chai = require('chai');
 var chaiAsPromised = require("chai-as-promised");
 const { expect, assert } = require('chai');
+const { createTodoItem } = require('../models/todos.js');
 
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('Todo list model', () => {
-    beforeEach('Clear DB and create three lists', async() => {
+    beforeEach('Clear DB and create three lists', async function() {
         await todoListModel.clearTodoListDatabase();
         for (let i = 0; i < 3; i++) {
             await todoListModel.createTodoList({
@@ -17,87 +18,52 @@ describe('Todo list model', () => {
         }
     });
 
-    it('createTodoList() & getTodoList() | Should create a todolist and find it', async() => {
+    it('createTodoList() | Should create a todolist and control it has the correct values', async function() {
         let createdTodoList = await todoListModel.createTodoList({
             title: 'TodoList with Mocha and Chai',
             ownerId: 'Testare'
         });
-        let findTodoList = await todoListModel.getTodoList(createdTodoList._id);
-        
         createdTodoList.should.be.an('object');
-        findTodoList.should.be.an('object');
-        (createdTodoList._id).should.be.equal(findTodoList._id);
-        (createdTodoList.title).should.be.equal(findTodoList.title);
-        (createdTodoList.ownerId).should.be.equal(findTodoList.ownerId);
+        expect(createdTodoList).to.deep.equal({
+            title: 'TodoList with Mocha and Chai',
+            ownerId: 'Testare',
+            _id: createdTodoList._id
+        });
     });
 
-    it('Should retrive an array with three todolists with the type object', async() => {
+    it('getTodoLists() | Should retrive an array with three todolists', async function() {
         let getListOfTodoLists = await todoListModel.getTodoLists();
         expect(getListOfTodoLists).to.be.an('array').with.lengthOf(3);
     });
 
-    it('createTodoList(), getTodoList() & updateTodoList() | Should create a todoList, find it and then update it, and finally find it again.', async() => {
+    it('updateTodoList() | Should create a todoList, then update it, and return 1 to indicated success on updating title field.', async function() {
         let createTodoList = await todoListModel.createTodoList({
             title: 'update this title, please I beg of you',
             ownerId: 'Fredde'
         });
-        let findCreatedTodoList = await todoListModel.getTodoList(createTodoList._id);
         let updateCreatedTodoList = await todoListModel.updateTodoList({
             title: 'Ah, much better title now'
-        }, findCreatedTodoList._id);
-        let findUpdatedTodoList = await todoListModel.getTodoList(createTodoList._id);
-
-        createTodoList.should.be.an('object');
-        (createTodoList.title).should.equal('update this title, please I beg of you');
-        (createTodoList.ownerId).should.equal('Fredde');
-
-        findCreatedTodoList.should.be.an('object');
-        (findCreatedTodoList._id).should.equal(createTodoList._id);
-        (findCreatedTodoList.title).should.equal('update this title, please I beg of you');
+        }, createTodoList._id);
 
         updateCreatedTodoList.should.be.equal(1);
-
-        (findUpdatedTodoList._id).should.equal(createTodoList._id);
-        (findUpdatedTodoList._id).should.equal(findCreatedTodoList._id);
-        (findUpdatedTodoList.title).should.equal('Ah, much better title now');
-        (findUpdatedTodoList.ownerId).should.equal('Fredde');
     });
 
-    it('countTodoLists() & clearTodoListDatabase() | Should count three objects in database, clear database and then count zero objects in database', async() => {
+    it('countTodoLists() | Should count three objects in database', async function() {
         let countTodoListsBeforeClearDB = await todoListModel.countTodoLists();
-        let clearTodoListDB = await todoListModel.clearTodoListDatabase();
-        let countTodoListsAfterClearDB = await todoListModel.countTodoLists();
-
 
         countTodoListsBeforeClearDB.should.be.an('number');
         countTodoListsBeforeClearDB.should.equal(3);
-
-        clearTodoListDB.should.be.an('number');
-        clearTodoListDB.should.equal(3);
-
-        countTodoListsAfterClearDB.should.be.an('number');
-        countTodoListsAfterClearDB.should.equal(0);
     });
 
-    it('createTodoList(), getTodoList() & deleteTodoList() | Should create a todolist, find the created todolist, remove it and then look for it and get no result from DB', async() => {
+    it('deleteTodoList() | Should create a todolist, remove it and return a 1, then look for it and get an error', async function() {
         let createTodoList = await todoListModel.createTodoList({
             title: 'I have a bad feeling about this...',
             ownerId: 'Foreshadowing'
         });
-        let getTodoList = await todoListModel.getTodoList(createTodoList._id);
-        let deleteTodoList = await todoListModel.deleteTodoList(getTodoList._id);
-        await expect(todoListModel.getTodoList(getTodoList._id)).to.be.rejectedWith(Error);
-
-        createTodoList.should.be.an('object');
-        (createTodoList.title).should.be.equal('I have a bad feeling about this...');
-        (createTodoList.ownerId).should.be.equal('Foreshadowing');
-
-        getTodoList.should.be.an('object');
-        (getTodoList.title).should.be.equal(createTodoList.title);
-        (getTodoList.ownerId).should.be.equal(createTodoList.ownerId);
-        (getTodoList._id).should.be.equal(createTodoList._id);
-
+        let deleteTodoList = await todoListModel.deleteTodoList(createTodoList._id);
+        
         deleteTodoList.should.be.an('number');
         deleteTodoList.should.be.equal(1);
+        await expect(todoListModel.getTodoList(createTodoList._id)).to.be.rejectedWith(Error);
     })
 });
