a2c1df40ee26efb2eb1cac536e17aaa5f31a29e1
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-09-08 15:39:36 +0200

    Added Arrange, Act and Assert on todoList.test.js

--- test/todoList.test.js
+++ test/todoList.test.js
@@ -18,10 +18,15 @@ describe('Todo list model', () => {
     });
 
     it('createTodoList() | Should create a todolist and control it has the correct values', async function() {
-        let createdTodoList = await todoListModel.createTodoList({
+        // Arrange
+        let newTodoList = {
             title: 'TodoList with Mocha and Chai',
             ownerId: 'Testare'
-        });
+        }
+        // Act
+        let createdTodoList = await todoListModel.createTodoList(newTodoList);
+        
+        // Assert
         createdTodoList.should.be.an('object');
         expect(createdTodoList).to.deep.equal({
             title: 'TodoList with Mocha and Chai',
@@ -30,39 +35,68 @@ describe('Todo list model', () => {
         });
     });
 
-    it('getTodoLists() | Should retrive an array with three todolists', async function() {
+    it('getTodoLists() | Should retrive an array with four todolists', async function() {
+        // Arrange
+        let newTodoList = {
+            title: 'TodoList with Mocha and Chai',
+            ownerId: 'Testare'
+        }
+        await todoListModel.createTodoList(newTodoList);
+
+        // Act
         let getListOfTodoLists = await todoListModel.getTodoLists();
-        expect(getListOfTodoLists).to.be.an('array').with.lengthOf(3);
+        
+        // Assert
+        expect(getListOfTodoLists).to.be.an('array').with.lengthOf(4);
     });
 
     it('updateTodoList() | Should create a todoList, then update it, and return 1 to indicated success on updating title field.', async function() {
-        let createTodoList = await todoListModel.createTodoList({
-            title: 'update this title, please I beg of you',
-            ownerId: 'Fredde'
-        });
+        // Arrange
+        let newTodoList = {
+            title: 'TodoList with Mocha and Chai',
+            ownerId: 'Testare'
+        }
+        let createdTodoList = await todoListModel.createTodoList(newTodoList);
+
+        // Act
         let updateCreatedTodoList = await todoListModel.updateTodoList({
             title: 'Ah, much better title now'
-        }, createTodoList._id);
+        }, createdTodoList._id);
 
+        // Assert
         updateCreatedTodoList.should.be.equal(1);
     });
 
-    it('countTodoLists() | Should count three objects in database', async function() {
-        let countTodoListsBeforeClearDB = await todoListModel.countTodoLists();
+    it('countTodoLists() | Should count four objects in database', async function() {
+        // Arrange
+        let newTodoList = {
+            title: 'TodoList with Mocha and Chai',
+            ownerId: 'Testare'
+        }
+        let createdTodoList = await todoListModel.createTodoList(newTodoList);
 
+        // Act
+        let countTodoListsBeforeClearDB = await todoListModel.countTodoLists();
+        
+        // Assert
         countTodoListsBeforeClearDB.should.be.an('number');
-        countTodoListsBeforeClearDB.should.equal(3);
+        countTodoListsBeforeClearDB.should.equal(4);
     });
 
     it('deleteTodoList() | Should create a todolist, remove it and return a 1, then look for it and get an error', async function() {
+        // Arrange
         let createTodoList = await todoListModel.createTodoList({
             title: 'I have a bad feeling about this...',
             ownerId: 'Foreshadowing'
         });
+        
+        // Act
         let deleteTodoList = await todoListModel.deleteTodoList(createTodoList._id);
         
+        // Assert
         deleteTodoList.should.be.an('number');
         deleteTodoList.should.be.equal(1);
         await expect(todoListModel.getTodoList(createTodoList._id)).to.be.rejectedWith(Error);
+        
     })
 });
