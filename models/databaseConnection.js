fd9f59bd55b2401b18f723160c08334517931627
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-09-16 16:05:11 +0200

    Added mongoose logic to databaseConnection.js

--- models/databaseConnection.js
+++ models/databaseConnection.js
@@ -1,3 +1,58 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let developmentDB, testDB;
+
+switch (process.env.ENVIROMENT) {
+    case 'development':
+        const {MongoMemoryServer} = require('mongodb-memory-server');
+        developmentDB = new MongoMemoryServer();
+        break;
+    case 'test':
+        const {MongoMemoryServer} = require('mongodb-memory-server')
+        testDB = new MongoMemoryServer();
+        break;
+
+        default:
+            console.log("Running databaseconnection on default");
+            break;
+        /*
+    case 'staging':
+        mongoDatabase = {
+            // mongodb+srv://user:password@host/dbname
+            getUri: async () => 
+                `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+        }
+        break;
+    case 'production':
+    
+        break;
+        */
+}
+
+async function connect(){
+    
+    let uri = await mongoDatabase.getUri()
+
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    })
+}
+
+async function disconnect(){
+    await mongoDatabase.stop()
+    await mongoose.disconnect()
+}
+
+
+module.exports = {
+    connect, disconnect
+}
+
+/*
 require('dotenv').config();
 const nedb = require('nedb');
 
@@ -24,3 +79,4 @@ module.exports = {
     usersDatabase,
     todoListDatabase
 };
+*/
