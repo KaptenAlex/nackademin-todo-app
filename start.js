2c5a9855804313eee0f2feb42ebdf8cddeeb4340
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-09-16 14:25:23 +0200

    Added npx to test script and a new .env variable for port

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const app = require('./app.js');
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
